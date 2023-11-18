0fc76870e0e81fbf742672a5b09bb0addbac7015
kishoribhalsing <138517223+kishoribhalsing@users.noreply.github.com>
2023-11-18 18:19:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Home from './components/Home';
+import NewPost from './components/Posts';
+
+
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <Home />
+      <Posts/>
+    </div>
+  );
+}
+
+export default App;
